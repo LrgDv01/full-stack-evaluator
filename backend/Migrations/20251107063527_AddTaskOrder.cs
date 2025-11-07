b181c0abab2ab7787d3ccd8fd66d361738cadf8d
LrgDv01 <01brnlrg.devengin.workemp@gmail.com>
2025-11-07 14:44:32 +0800

    + Ultimate Upgrade: Drag-and-drop reordering with @dnd-kit, dark mode with Tailwind dark:, persistent order in DB, full polish

---
+++ backend/Migrations/20251107063527_AddTaskOrder.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace task_manager_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTaskOrder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Order",
+                table: "Tasks",
+                nullable: false,
+                defaultValue: 0);
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
